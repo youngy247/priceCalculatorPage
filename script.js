73a3a9337a3b7e439d07ad78c2e7f24fb47fd833
Adam Young <adamyoungy678@gmail.com>
2023-04-14 12:08:02 +0100

    add billing cycle class in html

--- script.js
+++ script.js
@@ -4,27 +4,31 @@ const priceValue = document.querySelector('.price-value')
 const discountValue = document.querySelector('.discount')
 
 
-const prices = {
-    "10k": 8,
-    "50k": 12,
-    "100k": 16,
-    "500k": 24,
-    "1m": 36
-};
+
 
 let discountApplied = false
 
 function calculatePrice(){
-let pageViews = pageViewSlider.value
+let pageViews = parseInt(pageViewSlider.value);
+
+    const prices = {
+        "10k": 8,
+        "50k": 12,
+        "100k": 16,
+        "500k": 24,
+        "1m": 36
+    }
 
     let price = prices[`${
         pageViews < 50 ? pageViews + "k" : "1m"
     }`] * (discountApplied ? 0.75 : 1);
+    // console.log(pageViews)
+    priceValue.textContent = `$${price.toFixed(2)}`;
 
 }
-
+console.log(priceValue)
 billingSwitch.addEventListener('change', () => {
-    discountApplied = toggleSwitch.checked;
+    discountApplied = billingSwitch.checked;
     calculatePrice()
 })
 
